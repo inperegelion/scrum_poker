ab37bd298022766d9e2ae7c63bff5f9c436a7fa2
Serhii Perehonchuk <inperegelion@gmail.com>
2023-01-10 19:02:17 +0200

    api impoved client side routing support

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,17 +1,15 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import { connectMongo } from './db';
-
 import roomsRouter from './routers/rooms';
-import path from 'path';
 
 require('dotenv').config({ path: '../.env' });
 
 const PORT = process.env.API_RUNNING_PORT ?? 3001;
 const app = express();
-const CLIENT_BUILD_PATH = path.join(__dirname, '..', '..', 'client', 'build');
-
+// const CLIENT_BUILD_PATH = path.join(__dirname, '..', '..', 'client', 'build');
 connectMongo(process.env.MONGO_CONNECTION_STRING);
 
 // middlewares
@@ -22,7 +20,8 @@ app.use(express.json());
 app.use('/api/room', roomsRouter);
 
 // static & client
-app.use('/', express.static(CLIENT_BUILD_PATH));
+app.use('/', express.static('build'));
+app.use('/*', express.static('build'));
 
 app.listen(PORT);
 console.log(`👂 Listening on a port: ${PORT}`);
